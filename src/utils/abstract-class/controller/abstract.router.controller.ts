1d01165fc1c3b26a76e7438cef781a5a13c89ce6
Ygor Fortes <ygor1888@hotmail.com>
2024-01-29 18:05:48 -0300

    feat(abstract.router.controller.ts): created abstract class to define routes in controller

---
+++ src/utils/abstract-class/controller/abstract.router.controller.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+
+export abstract class AbstractRouterController {
+  protected router : Router;
+
+  constructor() {
+    this.router = Router();
+    this.setupRouter();
+  }
+
+  abstract setupRouter() : void; 
+
+  routes() : Router {
+    return this.router;
+  }
+
+  
+}
