88c6ca22f4274fb858dd6fe5e1d0aa1b7283f1e4
Ygor Fortes <ygor1888@hotmail.com>
2024-01-29 18:08:23 -0300

    feat(custom.http.error.ts): created class for custom error request with checkAndThrowError method

---
+++ src/erros/custom.http.error.ts
@@ -0,0 +1,22 @@
+export class CustomHttpError extends Error {
+  public  statusCode: number;
+
+  constructor(message = 'Problemas no servidor! Volte mais tarde', statusCode = 500) {
+    super(message);
+    this.message = message;
+    this.statusCode = statusCode;
+    this.name = CustomHttpError.name; 
+    Object.setPrototypeOf(this, CustomHttpError.prototype);
+  }
+
+  static checkAndThrowError(error: unknown): void {
+    if (error instanceof CustomHttpError) {
+      throw error;
+    }
+
+    throw new Error();
+  }
+
+}
+
+
