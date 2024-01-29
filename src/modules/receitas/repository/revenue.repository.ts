5d0393a04f172a8104f897ce84483200dbcaf40f
Ygor Fortes <ygor1888@hotmail.com>
2024-01-29 18:13:13 -0300

    feat(revenue.repository.ts) created repository for Revenue and started method of abstract class and method checkDuplicateDescriptInSameMonth

---
+++ src/modules/receitas/repository/revenue.repository.ts
@@ -0,0 +1,45 @@
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { CrudRepository } from '../../../utils/abstract-class/repository/abstract.crud.repository';
+import { Receitas } from '@prisma/client';
+import { CustomHttpError } from '../../../erros/custom.http.error';
+
+export class RevenueRepository extends CrudRepository<Receitas> {
+
+  async create(dataRevenue: Receitas): Promise<{ id: number; descricao: string; valor: number; data: Date; }> {
+    const newRevenue = await this.primaClient.receitas.create({ data: dataRevenue });
+
+    return newRevenue ;
+  }
+
+  findAll(filter: object): null[] | { id: number; descricao: string; valor: number; data: Date; }[] {
+    throw new Error('Method not implemented.');
+  }
+
+  findOne(elementId: number): { id: number; descricao: string; valor: number; data: Date; } | null {
+    throw new Error('Method not implemented.');
+  }
+
+  update(elementId: number, element: object): { id: number; descricao: string; valor: number; data: Date; } {
+    throw new Error('Method not implemented.');
+  }
+
+  delete(elementId: number): number {
+    throw new Error('Method not implemented.');
+  }
+  
+  async checkDuplicateDescriptionInSameMonth(descrition: string, date: Date): Promise<number> {
+    const duplicateDescrition = await  this.primaClient.receitas.count({
+      where: { 
+        descricao: descrition, 
+        data: {
+          gte: new Date(date.getFullYear(), date.getMonth(),  1),
+          lt: new Date(date.getFullYear(), date.getMonth() + 1,  1),
+        },
+      },
+    });
+    
+    return duplicateDescrition; 
+  }
+
+}
