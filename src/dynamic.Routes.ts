67372a10de15e8110b09a6838bee439585f6bd77
Ygor Fortes <ygor1888@hotmail.com>
2024-01-30 16:24:50 -0300

    feat(dynamic.Routes): placed method for handle errors 404 call handleError404

--- src/dynamic.Routes.ts
+++ src/dynamic.Routes.ts
@@ -23,6 +23,7 @@ export class DynamicRoutes {
     this.router.use('/receitas', revenueController.routes());
 
     this.router.use(errorMiddleware.handleRequestErros());
+    this.router.use(errorMiddleware.handleErro404());
   }
 
   public attachToApp(app: Application): void {
