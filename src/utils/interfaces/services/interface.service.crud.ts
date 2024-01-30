2d5e944e46a2edd29902a4cc6ee27ae56a758342
Ygor Fortes <ygor1888@hotmail.com>
2024-01-30 16:32:13 -0300

    chore(interface.service.crud.ts): method abstract update placed in parameter generic T

--- src/utils/interfaces/services/interface.service.crud.ts
+++ src/utils/interfaces/services/interface.service.crud.ts
@@ -2,11 +2,11 @@ export interface InterfaceCrudService<T> {
 
   findAll(filter: object): Promise<T[] | undefined>;
 
-  findOne(elementId: number) : Promise<T>;
+  findOne(elementId: number) : Promise<T | undefined>;
 
   create(element: T) :Promise<T | undefined>;
 
-  update(elementId: number, element: T): Promise<T>;
+  update(elementId: number, element: T): Promise<T | undefined>;
 
   delete(elementId: number) : Promise<T>;
 
