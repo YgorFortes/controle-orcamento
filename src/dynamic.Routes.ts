e82d76565ecf5775e9c00bea2d4b3b33cbc187a1
Ygor Fortes <ygor1888@hotmail.com>
2024-01-31 15:53:21 -0300

    feat(dynamic.Routes): instantiated expense controller and created routes /despesas

--- src/dynamic.Routes.ts
+++ src/dynamic.Routes.ts
@@ -2,6 +2,7 @@ import express, { Application, Router, json } from 'express';
 import { AppController } from './modules/app/app.controller';
 import { RevenueController } from './modules/revenue/revenue.controller'; 
 import { ErrorMiddleware } from './middlewares/error.middewares';
+import { ExpenseController } from './modules/expenses/expense.controller';
 
 export class DynamicRoutes {
   protected router: express.Router;
@@ -15,12 +16,14 @@ export class DynamicRoutes {
     this.router.use(json());
 
     // Instanciar controladores e definir rotas
+    const errorMiddleware = new ErrorMiddleware();
     const appController = new AppController();
     const revenueController = new RevenueController();
-    const errorMiddleware = new ErrorMiddleware();
+    const expensesController = new ExpenseController();
 
     this.router.use('/', appController.routes());
     this.router.use('/receitas', revenueController.routes());
+    this.router.use('/despesas', expensesController.routes());
 
     this.router.use(errorMiddleware.handleRequestErros());
     this.router.use(errorMiddleware.handleError404());
