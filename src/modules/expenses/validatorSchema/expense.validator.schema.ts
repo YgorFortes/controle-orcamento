c6f8d67961671a39a64cd38410463332fd4b5738
Ygor Fortes <ygor1888@hotmail.com>
2024-01-31 17:24:07 -0300

    feat(expense.validator.schema): created validation for query params for findAll in controller expenses

--- src/modules/expenses/validatorSchema/expense.validator.schema.ts
+++ src/modules/expenses/validatorSchema/expense.validator.schema.ts
@@ -14,4 +14,14 @@ export class ExpenseValidatorSchema {
     const result = await expenseBodyValidator.validate(body);
     return result  ;
   }
+
+  public async findAll(query: object) : Promise<object> {
+    const revenueSchemaFindAll = object({
+      page: number().typeError('O campo page só recebe números.').integer('O campo page só recebe números inteiros.').positive('O campo page só recebe números positivos'),
+      limit: number().typeError('O campo limit só recebe números.').integer('O campo limit só recebe números inteiros.').positive('O campo limit só recebe números positivos'),
+    }).noUnknown();
+
+    const resultRevenueSchemaFindAll = await revenueSchemaFindAll.validate(query);
+    return resultRevenueSchemaFindAll;
+  }
 }
