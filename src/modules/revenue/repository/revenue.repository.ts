695dfdf490c253079bd4b3ecc1021d349b3da0fd
Ygor Fortes <ygor1888@hotmail.com>
2024-01-31 15:59:11 -0300

    chore: chanced ValidatorSchemaReceita for revenue.validator.schema

--- src/modules/revenue/repository/revenue.repository.ts
+++ src/modules/revenue/repository/revenue.repository.ts
@@ -1,5 +1,3 @@
-
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { CrudRepository } from '../../../utils/abstract-class/repository/abstract.crud.repository';
 import { Receitas } from '@prisma/client';
 
