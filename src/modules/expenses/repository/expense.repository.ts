603d549efce23ca93d5c7d9a25a3de549a7196fc
Ygor Fortes <ygor1888@hotmail.com>
2024-02-06 09:31:59 -0300

    refac: retired constants and all method return directy

--- src/modules/expenses/repository/expense.repository.ts
+++ src/modules/expenses/repository/expense.repository.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { CustomHttpError } from '../../../erros/custom.http.error';
 import { CrudRepository } from '../../../utils/abstract-class/repository/abstract.crud.repository';
 import { Despesas } from '@prisma/client';
 
@@ -7,57 +8,53 @@ export class ExpenseRepository  extends CrudRepository<Despesas> {
   public async findAll(filter?:  {  page?: number, limit?: number, descricao?: string }): Promise<Array<Despesas>> {
     const { page = 1, limit = 10, descricao } = filter ?? {};
 
-    const listExpenses = await this.primaClient.despesas.findMany({
+    if (limit > 100) {
+      throw new CustomHttpError('O limite máximo permitido para a pesquisa é de 100 registros por página.', 400);
+    }
+
+    return this.primaClient.despesas.findMany({
       take: limit,
       skip: (page - 1) * limit,
       where: descricao ? { descricao: { contains: descricao } } : undefined,
     });
-
-    return listExpenses;
   }
  
 
   public async findOne(elementId: number): Promise<Despesas | null> {
-    const expenseDetails = await this.primaClient.despesas.findUnique({
+    return this.primaClient.despesas.findUnique({
       where: {
         id: elementId,
       },
     });
-
-    return expenseDetails;
   }
 
   
-
   public async create(dataExpense: Despesas): Promise<Despesas> {
-    const newExpense = await this.primaClient.despesas.create({
+    return  this.primaClient.despesas.create({
       data: dataExpense,
     });
-
-    return newExpense;
   }
 
+
   public async update(elementId: number, element: Despesas): Promise<Despesas> {
-    const newIntoExpense = await this.primaClient.despesas.update({
+    return  this.primaClient.despesas.update({
       where: { id: elementId },
       data: {
         ...element,
       },
     });
-
-    return newIntoExpense;
   }
+
 
   public async delete(elementId: number): Promise<Despesas> {
-    const resultDeleteExpanse = await this.primaClient.despesas.delete({
+    return this.primaClient.despesas.delete({
       where: { id: elementId },
     });
-
-    return resultDeleteExpanse;
   }
 
+
   async checkDuplicateDescriptionInSameMonth(descrition: string, date: Date): Promise<number> {
-    const duplicateDescrition = await this.primaClient.despesas.count({
+    return this.primaClient.despesas.count({
       where: {
         descricao: descrition,
         data: {
@@ -66,11 +63,6 @@ export class ExpenseRepository  extends CrudRepository<Despesas> {
         },
       },
     });
-
-    return duplicateDescrition;
   }
-
-
-  
 
 }
