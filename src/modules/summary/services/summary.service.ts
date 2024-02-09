ef689559b8c71298b60f4f3c817fe08773cd6a52
Ygor Fortes <ygor1888@hotmail.com>
2024-02-09 15:20:04 -0300

    feat: now summary creates one summary for the month with revenue and expense
    created methods called totalForCategory, calculateTotalValue, and summary

---
+++ src/modules/summary/services/summary.service.ts
@@ -0,0 +1,87 @@
+import { ExpenseService } from '../../expenses/services/expenses.services';
+import { RevenueService } from '../../revenue/services/revenue.services';
+import { CustomHttpError } from '../../../erros/custom.http.error';
+import { InterfaceSummarySearchOptions } from '../../../utils/interfaces/validators/sumarySchema.interface';
+import { Despesas } from '@prisma/client';
+
+
+export class  SummaryService {
+  private revenueService: RevenueService;
+
+  private expenseService: ExpenseService;
+
+  constructor() {
+    this.revenueService = new RevenueService();
+    this.expenseService = new ExpenseService();
+  }
+
+  public async sumary(summaryDates: InterfaceSummarySearchOptions): Promise<any> {
+    try {
+      const revenues = await this.revenueService.findRevenueByMonth(summaryDates);
+
+      const expenses = await this.expenseService.findRevenueByMonth(summaryDates);
+
+      if (!revenues || !expenses) {
+        throw new CustomHttpError('Não encontrado receitas ou despesas nessse mês.', 200);
+      }
+
+      const totalRevenues = this.calculateTotalValue(revenues);
+      const totalExpenses = this.calculateTotalValue(expenses);
+      
+      const finalBalance = this.calculateFinalBalance(totalRevenues, totalExpenses);
+
+
+      const totalExpenseByCategory = this.totalForCategory(expenses);
+      
+      return { 
+        valorTotalReceitas: totalRevenues, 
+        valorTotalDespesas: totalExpenses,  
+        saldoFinalDoMes: finalBalance, 
+        totalPorCategoria: totalExpenseByCategory,
+      };
+
+    } catch (error) {
+      CustomHttpError.checkAndThrowError(error);
+    }
+  }
+
+  /**
+   * Calculates the total value of revenues or expenses for a given month.
+   * @template T - Type of the array elements, must have a 'valor' property of type number.
+   * @param {Array<T>} array - An array of objects representing revenues or expenses.
+   * @returns {number | undefined} - The total value of revenues or expenses for the given month, or undefined if an error occurs.
+  */
+  private calculateTotalValue<T extends { valor: number }>(array: Array<T>): number {
+    const totalByMonth = array.reduce((accumulator, currentItem) => accumulator + currentItem.valor, 0);
+
+    return totalByMonth;
+  }
+
+  /**
+   * Calculates the final balance for the month based on the total revenues and total expenses.
+   * 
+   * @param {number} totalRevenues - Total revenues for the month.
+   * @param {number} totalExpenses - Total expenses for the month.
+   * @returns {number} - The final balance for the month.
+  */
+  private calculateFinalBalance(totalRevenues: number, totalExpenses: number): number {
+    const finalBalance = totalRevenues - totalExpenses;
+    return finalBalance;
+  } 
+
+  private totalForCategory(expenses: Array<Despesas>): object {
+
+    const totalForCategory: { [categoria: string]: number } = {};
+      
+    for (const expense of expenses) {
+      if (!totalForCategory[expense.categoria]) {
+        totalForCategory[expense.categoria] = 0;
+      }
+
+      totalForCategory[expense.categoria] += expense.valor; 
+    }
+
+    return totalForCategory;
+  
+  }
+}
---
+++ src/modules/summary/summary.controller.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from 'express';
+import { AbstractRouterController } from '../../utils/abstract-class/controller/abstract.router.controller';
+import { SummaryValidatorSchema } from './validatorSchema/summarySchema.validator';
+import { SummaryService } from './services/summary.service';
+import { InterfaceSummarySearchOptions } from '../../utils/interfaces/validators/sumarySchema.interface';
+
+export class SummaryController extends AbstractRouterController {
+  private summaryValidatorSchema: SummaryValidatorSchema;
+
+  private summaryService: SummaryService;
+
+  constructor() {
+    super();
+    this.summaryValidatorSchema = new SummaryValidatorSchema();
+    this.summaryService = new SummaryService();
+  }
+
+  setupRouter(): void {
+    this.summary();
+  }
+
+  summary(): void {
+    this.router.get('/:ano/:mes', async (req: Request, res: Response, next: NextFunction )=>{
+      try {
+        const summaryValidated = await this.summaryValidatorSchema.validateSumary(req.params, req.query) as InterfaceSummarySearchOptions;
+
+        const summary = await this.summaryService.sumary(summaryValidated);
+        return res.status(200).send(summary);
+      } catch (error) {
+        next(error);
+      } 
+    });
+  }
+  
+}
