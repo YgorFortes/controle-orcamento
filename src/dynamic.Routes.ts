8fdc04d70ca1636cf7c9e9a02255c7ea3cccb386
Ygor Fortes <ygor1888@hotmail.com>
2024-02-16 17:00:37 -0300

    feat: intancianting userController routes

--- src/dynamic.Routes.ts
+++ src/dynamic.Routes.ts
@@ -4,6 +4,7 @@ import { RevenueController } from './modules/revenue/revenue.controller';
 import { ErrorMiddleware } from './middlewares/error.middewares';
 import { ExpenseController } from './modules/expenses/expense.controller';
 import { SummaryController } from './modules/summary/summary.controller';
+import { UserController } from './modules/user/user.controller';
 
 export class DynamicRoutes {
   protected router: express.Router;
@@ -22,8 +23,10 @@ export class DynamicRoutes {
     const revenueController = new RevenueController();
     const expensesController = new ExpenseController();
     const summaryController = new SummaryController();
+    const userController = new UserController();
 
     this.router.use('/', appController.routes());
+    this.router.use('/', userController.routes());
     this.router.use('/receitas', revenueController.routes());
     this.router.use('/despesas', expensesController.routes());
     this.router.use('/resumo', summaryController.routes());
